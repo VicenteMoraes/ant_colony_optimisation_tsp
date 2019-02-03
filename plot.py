7935f2abbb09eeb8c687d257adfa090bfc7c1661
VicenteMoraes <olifus@hotmail.com>
2019-02-03 19:38:03 -0200

    Changes to be committed: 	modified:   main.py 	modified:   plot.py

--- plot.py
+++ plot.py
@@ -1,11 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def graph(nodes, matrix):
+def graph(nodes, matrix, names):
     distance = [matrix[x][x + 1] for x in range(len(nodes)-1)]
     distance.append(0)
-    color = ["w" if nodes.index(x) == 0 else "b" if nodes.index(x) == len(nodes)-1 else "r" for x in nodes]
-    plt.plot(distance, nodes, marker="o", markersize=30, markerfacecolor="r")
+    plt.plot(distance,nodes, zorder=1, label="Links", color="r")
+    plt.scatter(distance[0], nodes[0], marker="o", color="green", s=20 ** 2, zorder=2, label="Start")
+    plt.scatter(distance[1:-1], nodes[1:-1], marker="o", color="blue", s=20 ** 2, zorder=2, label="Nodes")
+    plt.scatter(distance[-1], nodes[-1], marker="o", color="red", s=20 ** 2, zorder=2, label="End")
+    for x in range(len(distance)):
+        plt.annotate(names[nodes[x]], (distance[x], nodes[x]))
+    plt.legend()
     plt.xlabel("Distance")
     plt.ylabel("Node Number")
     plt.show()
